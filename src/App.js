eb5f4a780a4b99b2a70a9646c9ecdceab88ec56d
gusfahmi <gus@entertrim.com>
2021-06-14 19:14:38 +0700

    Changin name file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Index from "./Screens/Index";
+import Home from "./Screens/Home";
 import Services from "./Screens/Services";
 import About from "./Screens/About";
 import Contacts from "./Screens/Contacts";
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path='/' component={Index} exact />
+				<Route path='/' component={Home} exact />
 				<Route path='/services' component={Services} exact />
 				<Route path='/about' component={About} exact />
 				<Route path='/contact' component={Contacts} exact />
