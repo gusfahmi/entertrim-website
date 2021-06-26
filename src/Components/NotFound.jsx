f1566050e6e02ddbefaa964d2c54b7ebf3c4e631
gusfahmi <gus@entertrim.com>
2021-06-26 21:18:12 +0700

    Updating 404 design

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import Header from "./Header";
-import Footer from "./Footer";
 
 export default function NotFound() {
 	return (
 		<>
 			<Header />
 
-			<div className='container-fluid'>
-				<div className='wrap-not-found d-flex justify-content-center align-items-center'>
-					<img src='/assets/icons/notfound.svg' alt='' />
-				</div>
+			<div className='wrap-not-found d-flex flex-column justify-content-center align-items-center'>
+				<h2>404 | NOT FOUND</h2>
+				<div className='border-404'></div>
+				<Link to='/'>
+					<button className='btn btn-gold btn-not-found'>HOME</button>
+				</Link>
 			</div>
-
-			<Footer />
 		</>
 	);
 }
