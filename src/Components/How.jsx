825f27dd9b73d1872cf94a3feeef9ac2e444d338
gusfahmi <gus@entertrim.com>
2021-06-30 23:50:08 +0700

    Updating web design

--- src/Components/How.jsx
+++ src/Components/How.jsx
@@ -1,25 +1,49 @@
 import React from "react";
 import { setLang } from "../lib/Language.js";
+import { Link } from "react-router-dom";
 
 export default function How() {
 	return (
 		<>
 			<div className='container-fluid'>
-				<section className='wrapper-help'>
+				{/* <section className='wrapper-help'>
 					<img
 						className='icon-index'
 						src='/assets/icons/help.svg'
 						alt='How we can help you | Entertrim'
 					/>
 					<h3 className='text-center text-dark mb-5 mt-4 text-title-explain'>
-						{/* <span>HOW CAN WE HELP YOU</span> */}
 						<span>{setLang().How_Title}</span>
 					</h3>
 					<p className='text-center'>{setLang().How_Content}</p>
-				</section>
-			</div>
+				</section> */}
 
-			<div className='border-bg-black'></div>
+				<div className='wrapper-how'>
+					<div className='row justify-content-center'>
+						<div className='col-lg-5'>
+							<div className='content-how-description'>
+								<div className='left-line-how mr-3'></div>
+								<div className='right-content-how'>
+									<h3>
+										<span>{setLang().How_Title}</span>
+									</h3>
+									<p>{setLang().How_Content}</p>
+									<Link
+										to='/services'
+										className='btn btn-gold-services'>
+										Layanan kami
+									</Link>
+								</div>
+							</div>
+						</div>
+						<div className='col-lg-7'>
+							<div className='content-image-how'>
+								<img src='/assets/icons/business.svg' alt='' />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/Components/Reasons.jsx
+++ src/Components/Reasons.jsx
@@ -16,7 +16,7 @@ export default function Reasons() {
 					</h3>
 				</section>
 
-				<section className='wrapper-detail-choose'>
+				{/* <section className='wrapper-detail-choose'>
 					<div className='row justify-content-center'>
 						<div className='col-lg-3 col-md-5 col-sm-12 m-2 mb-lg-5'>
 							<div className='card background-dark list-choose'>
@@ -115,6 +115,136 @@ export default function Reasons() {
 									alt='Experience | Entertrim Technology'
 								/>
 								<div className='card-body'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Experience}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Experience_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section> */}
+
+				<section className='wrapper-detail-choose'>
+					<div className='row justify-content-center p-3'>
+						<div className=' col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='list-choose background-dark '>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/domain.svg'
+										alt='Free Domain | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Free_Domain}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Free_Domain_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='background-dark list-choose'>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/hosting.svg'
+										alt='Free Hosting | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Free_Hosting}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Free_Hosting_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='background-dark list-choose'>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/ssl.svg'
+										alt='Free SSL | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Free_SSL}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Free_SSL_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='background-dark list-choose'>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/consultant.svg'
+										alt='Free Consultant | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Free_Consultant}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Free_Consultant_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='background-dark list-choose'>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/guaranteed.svg'
+										alt='Guarantee | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
+									<h5 className='card-title'>
+										{setLang().Reasons_Guarantee}
+									</h5>
+									<p className='card-text'>
+										{setLang().Reasons_Guarantee_Description}
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div className='col-lg-4 col-md-6 p-2 col-reason'>
+							<div className='background-dark list-choose'>
+								<div className='wrap-img-reason'>
+									<img
+										className='card-img-top img-card-reason'
+										src='/assets/icons/trusted.svg'
+										alt='Experience | Entertrim Technology'
+									/>
+								</div>
+
+								<div className='content-reason'>
 									<h5 className='card-title'>
 										{setLang().Reasons_Experience}
 									</h5>
